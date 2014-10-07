0dabbc130e92863a7e94f81ff68ef9f07835fdd1
kavitshah8 <kavit.shah8@gmail.com>
2014-10-06 23:43:10 -0400

    updates restadapter, removes deadcode

--- app/adapters/subreddit.js
+++ app/adapters/subreddit.js
@@ -4,38 +4,6 @@ import Ember from 'ember';
 export default Ember.Object.extend({
 
 	getJSONWithoutToken:function(url){
-		return ajax({url:url}).then(function(result){
-			return result.data.children.map(function(c){
-				return {
-					id: c.id,
-					title: c.data.title,
-					domain: c.data.domain,
-					url: c.data.url,
-					thumbnail:c.data.thumbnail,
-					author:c.data.author
-				};
-			});
-		});
-	},
-
-	find:function(name,id){
-		return ajax({ url: "http://www.reddit.com/r/"+id+".json", dataType: "json",type: 'GET' })
-				.then(function(result){
-					return result.data.children.map(function(c){
-						return {
-							id: c.id,
-							title: c.data.title,
-							domain: c.data.domain,
-							url: c.data.url,
-							thumbnail:c.data.thumbnail,
-							author:c.data.author
-						};
-					});
-				});
-	},
-
-	findGit:function(name){
-		return ajax({ url: "https://github.com/users/"+ name , dataType: "json"});
+		return ajax({url:url});
 	}
-
 });
--- app/routes/subreddit.js
+++ app/routes/subreddit.js
@@ -6,7 +6,18 @@ export default Ember.Route.extend({
 	model:function(params){
 		var adapter = SubredditAdapter.create();
 		var url = "http://www.reddit.com/r/"+params.subreddit_id+".json";
-		return adapter.getJSONWithoutToken(url);
-		// return adapter.find('subreddit',params.subreddit_id);
+
+		return adapter.getJSONWithoutToken(url).then(function(result){
+			return result.data.children.map(function(c){
+				return {
+					id: c.id,
+					title: c.data.title,
+					domain: c.data.domain,
+					url: c.data.url,
+					thumbnail:c.data.thumbnail,
+					author:c.data.author
+				};
+			});
+		});
 	}
 });
