8f7079a9bf7c9cc8cb4dd47e9eaead9ed95f545c
kavitshah8 <kavit.shah8@gmail.com>
2014-10-26 00:41:52 -0400

    modifies JSON before returnin, update template accordingle

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -6,10 +6,23 @@ export default Ember.Route.extend({
 	model: function(){
 		var adapter = Adapter.create();
 		var query = '%20item%20from%20weather.forecast%20where%20location%3D%2222102%22&';
-		var url = 'http://query.yahooapis.com/v1/public/yql?q=select'+query+'format=json';
+		var url = 'http://query.yahooapis.com/v1/public/yql?q=select' + query + 'format=json';
 
 		return adapter.getJSONWithoutToken(url).then(function(result){
-			return result.query.results.channel.item;
+			var title = result.query.results.channel.item.title.split(' ');
+			title = title[2] + title[3];
+
+			return {
+				title: title,
+				temp: result.query.results.channel.item.condition.temp,
+				text: result.query.results.channel.item.condition.text,
+				forecast: result.query.results.channel.item.forecast,
+			};
 		});
+	},
+
+	setupController: function(controller, model){
+		controller.set('model',model);
 	}
+
 });
