759d9f4711a51b66ea6f3774cbae2741136b7b6f
kavitshah8 <kavit.shah8@gmail.com>
2014-10-31 23:52:44 -0400

    adds missing JS for component

--- app/components/auto-complete.js
+++ app/components/auto-complete.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+	searchText: null,
+
+	dictionary: [],
+
+	results: function () {
+				
+		var searchText = this.get('searchText');
+		if(!searchText) {
+			return [];
+		}
+
+		var regex = new RegExp(searchText,'i');
+
+		// var res = this.get('dictionary').filter(function(word){
+		// 	return word.match(regex);
+		// });
+
+		// return res;
+
+		return this.get('dictionary').filter(function(word){
+			return word.match(regex);
+		}); 
+	}.property('searchText')
+
 });
