2c339a24237dbed14f425fa612bd34e9fffdff55
kavitshah8 <kavit.shah8@gmail.com>
2014-11-15 21:57:14 -0500

    client-side-authentication-part-1

---
+++ app/controllers/login.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+import Adapter from 'reddit/adapters/rest';
+
+export default Ember.Controller.extend({
+
+	reset: function(){
+		this.setProperties({
+			username: "",
+			password: "",
+			errorMessage:""
+		});
+	},
+
+	login: function(){
+
+		var self = this;
+		var adapter = Adapter.create();
+		var data = this.getProperties('username','password');
+
+		self.set('errorMessage', null);
+
+		return adapter.postWithoutToken('/api/auth.json', data).then(function(response){
+			self.set('errorMessage', response.message);
+			if (response.success){	
+				self.set('token', response.token);
+			}
+			return alert("response");
+		});		
+
+	}
+});
